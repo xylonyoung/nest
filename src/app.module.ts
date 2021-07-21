79aeb8f9b9f85b83bade7c08432a60afc8239b0d
xylon <xylonyoung@live.com>
2021-07-21 17:24:59 +0800

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+
+@Module({
+  imports: [CatsModule],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCatDto } from 'src/cats/dto/create-cat.dto';
+import { Cat } from 'src/cats/interfaces/cat.interface';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+}
